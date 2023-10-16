5a63b1da530fab0dedbb3aa76294fa0756fdfaab
Novi Sushmita <143358890+novisushmita@users.noreply.github.com>
2023-10-16 23:22:41 +0700

    Delete tugas7.py

--- tugas7.py
+++
@@ -1,9 +0,0 @@
-def show_data():
-    if len(buku) <= 0:
-        print("belum ada data")
-    else:
-        for indeks in range(len(buku)):
-            print("[%d] %s" % (indeks, buku[indeks]))
-
-buku = ["ayah", "ibu", "kakak", "adek"]
-show_data()
